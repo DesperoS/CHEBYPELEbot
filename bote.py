6220def64cbeec884d1c5c08127307eb6cf7bf14
DesperoS <evgeniy_ponomarev_02@mail.ru>
2020-03-21 22:53:22 +0300

    добваили файл для херОКУ

--- bote.py
+++ bote.py
@@ -3,9 +3,12 @@ import config
 
 bot = telebot.TeleBot(config.TOKEN)
 
+
 @bot.message_handler(content_types=['text'])
 def lalala(message):
 	bot.send_message(message.chat.id, message.text)
+	print(message.chat.username)
+
 
 #RUN
 bot.polling(none_stop=True)
