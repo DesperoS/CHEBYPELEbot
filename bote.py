19fe503b825a574ac34f8618c7150a30092df942
TheTOXIN <thetoksin@gmail.com>
2020-03-22 00:21:39 +0300

    бьюти

--- bote.py
+++ bote.py
@@ -2,48 +2,47 @@ import telebot
 import config
 import pyowm
 
-
 bot = telebot.TeleBot(config.TOKEN)
 owm = pyowm.OWM('9e74b4669f6a8cf98cab1138c031c5fb', language="ru")
 
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
-	user_text = message.text.lower()
+    user_text = message.text.lower()
 
-	print(message.chat.username, user_text)
+    print(message.chat.username, user_text)
 
-	observation = find_observation(user_text)
+    observation = find_observation(user_text)
 
-	if observation is None:
-		bot.send_message(message.chat.id, "Напиши нормальный город блять")
-		return
+    if observation is None:
+        bot.send_message(message.chat.id, "Напиши нормальный город блять")
+        return
 
-	w = observation.get_weather()
-	temp = w.get_temperature('celsius')["temp"]
+    w = observation.get_weather()
+    temp = w.get_temperature('celsius')["temp"]
 
-	first_message = "В городе " + user_text + " сейчас " + w.get_detailed_status()
-	second_message = "Температура сейчас в районе " + str(temp) + " градусов цельсия"
+    first_message = "В городе " + user_text + " сейчас " + w.get_detailed_status()
+    second_message = "Температура сейчас в районе " + str(temp) + " градусов цельсия"
 
-	bot.send_message(message.chat.id, first_message)
-	bot.send_message(message.chat.id, second_message)
+    bot.send_message(message.chat.id, first_message)
+    bot.send_message(message.chat.id, second_message)
 
-	sovet_message = "ни ебу"
+    sovet_message = "ни ебу"
 
-	if temp < 10:
-		sovet_message = "ОДЕВАЙ ПОДШТАННИКИ, ДЕБИЛ. Тебе ещё детей ростить"
-	elif temp > 10:
-		sovet_message = "чо нарядился как капуста, пиздуй в шортанах девок клеить и пиццу жрать, дебил"
+    if temp < 10:
+        sovet_message = "ОДЕВАЙ ПОДШТАННИКИ, ДЕБИЛ. Тебе ещё детей ростить"
+    elif temp > 10:
+        sovet_message = "чо нарядился как капуста, пиздуй в шортанах девок клеить и пиццу жрать, дебил"
 
-	bot.send_message(message.chat.id, sovet_message)
+    bot.send_message(message.chat.id, sovet_message)
 
 
 def find_observation(place):
-	try:
-		return owm.weather_at_place(place)
-	except:
-		return None
+    try:
+        return owm.weather_at_place(place)
+    except:
+        return None
 
 
-#RUN
+# RUN
 bot.polling(none_stop=True)
